e171960920abbb527de3eab807fbcd55de71b6e1
xperrin <xperrin@student.42.fr>
2018-08-08 19:47:46 +0200

    (lib update)separated printf header

--- includes/libft_test.h
+++ includes/libft_test.h
@@ -6,15 +6,17 @@
 /*   By: xperrin <xperrin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/28 01:07:20 by xperrin           #+#    #+#             */
-/*   Updated: 2018/05/28 02:05:32 by xperrin          ###   ########.fr       */
+/*   Updated: 2018/08/08 19:47:30 by xperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_TEST_H
 # define LIBFT_TEST_H
 
-#include "libft.h"
+# include "libft.h"
+# include "printf.h"
 
-int			test_sort(void (*sort)(void *, size_t, size_t, int (*)(const void *, const void *)));
+int		test_sort(void (*sort)(void *,
+			size_t, size_t, int (*)(const void *, const void *)));
 
 #endif
